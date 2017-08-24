bc39ade9f91b600cd181ace8c6d816fc133ef5fe
Artem Solomatin <artemsolomatin1905@gmail.com>
2017-08-24 22:32:54 +0300

    third

--- Authorization/src/main/resources/database.sql
+++ Authorization/src/main/resources/database.sql
@@ -1,15 +1,17 @@
 -- Table: users
 CREATE TABLE users (
-  id       SERIAL          NOT NULL PRIMARY KEY,
+  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
-);
+)
+  ENGINE = InnoDB;
 
 -- Table: roles
 CREATE TABLE roles (
-  id   SERIAL          NOT NULL PRIMARY KEY,
+  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL
-);
+)
+  ENGINE = InnoDB;
 
 -- Table for mapping user and roles: user_roles
 CREATE TABLE user_roles (
@@ -20,11 +22,12 @@ CREATE TABLE user_roles (
   FOREIGN KEY (role_id) REFERENCES roles (id),
 
   UNIQUE (user_id, role_id)
-);
+)
+  ENGINE = InnoDB;
 
 -- Insert data
 
-INSERT INTO users VALUES (1, 'tester', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
+INSERT INTO users VALUES (1, 'proselyte', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
 
 INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
